ffcbe04e98e4d528d9267f77e536f87ac4b60454
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2023-12-20 22:09:11 -0300

    downloaded and imported plugins

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+const obfuscate = require("gulp-obfuscate");
+
+
+function comprimeImg() {
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
+}
+
+exports.default = function() {
+    gulp.watch('./source/images/*', { ignoreInitial: false}, gulp.series(comprimeImg));
+}
