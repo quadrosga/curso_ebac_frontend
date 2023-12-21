1bf4105fac094467c62ed5e8923fd601d9dc7aa4
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2023-12-20 22:19:58 -0300

    added tasks and exports

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,25 @@ function comprimeImg() {
     .pipe(gulp.dest('./build/images'));
 }
 
+function comprimeJS() {
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest('./build/scripts'))
+}
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./build/styles'))
+}
+
 exports.default = function() {
     gulp.watch('./source/images/*', { ignoreInitial: false}, gulp.series(comprimeImg));
+    gulp.watch('./source/scripts/*.js', { ignoreInitial: false}, gulp.series(comprimeJS));
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false}, gulp.series(compilaSass));
 }
